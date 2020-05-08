1be404323abd5ad03ddcb2fa1a92aec3c2ada1db
Bartosz Keskrawiec <bartosz_keskrawiec@wp.pl>
2020-05-08 22:12:50 +0200

    let's start from the beginning

---
+++ src/main/java/com/sda/special_tools/SpecialToolsApplication.java
@@ -0,0 +1,13 @@
+package com.sda.special_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialToolsApplication.class, args);
+	}
+
+}
